64bc851f50438a91ee47990c82b1c102ad71c726
651890297 <651890fuzejun>
2020-02-02 22:07:06 +0800

    init prpo

---
+++ src/test/java/cn/fu/community/CommunityApplicationTests.java
@@ -0,0 +1,15 @@
+package cn.fu.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
